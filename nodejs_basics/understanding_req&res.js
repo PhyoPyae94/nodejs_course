28540366c8810a5af2ba3ff4a01fcf7a8eef8b12
PhyoPyae94 <kirudozack@gmail.com>
2019-02-22 11:10:52 +0630

    understanding request and responses

---
+++ nodejs_basics/understanding_req&res.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const port =3000;
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
+    // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>nodejs server running</title></head>');
+    res.write('<body><h1>Hello From Nodejs Server</h1></body>');
+    res.write('</html>');
+
+});
+
+server.listen(port);
