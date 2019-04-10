f40d3b1dc18d3946b92a62b5fcb4b11364b39ba4
PhyoPyae94 <kirudozack@gmail.com>
2019-04-10 23:16:36 +0630

    sending res, how middleware works

--- workingWithExpressJS/app.js
+++ workingWithExpressJS/app.js
@@ -7,10 +7,11 @@ const app = express();
 
 app.use((req, res, next) => {
     console.log('in the middleware');
-    next(); //along the request to continue to the next middleware in line
+    next(); //allow the request to continue to the next middleware in line
 });
 app.use((req, res, next) => {
     console.log('in the another middleware');
+    res.send('<h1>Hello express_js!</h1>');// send() allow us to send well a response
 });
 // use method allow us to add new middle ware function
 // the third argument, next is a function, a function that will be passed to this function by exprss_js
