02e82f5e400ad8e97561d1b2602c5e2e7a84a068
PhyoPyae94 <kirudozack@gmail.com>
2019-04-10 22:52:19 +0630

    adding middleware

--- workingWithExpressJS/app.js
+++ workingWithExpressJS/app.js
@@ -5,6 +5,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('in the middleware');
+    next(); //along the request to continue to the next middleware in line
+});
+app.use((req, res, next) => {
+    console.log('in the another middleware');
+});
+// use method allow us to add new middle ware function
+// the third argument, next is a function, a function that will be passed to this function by exprss_js
 const server = http.createServer(app);
 
 server.listen(port);
