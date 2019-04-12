e99c4550f4132b16e424830df1dc9dff45de0920
PhyoPyae94 <kirudozack@gmail.com>
2019-04-13 00:04:40 +0630

    filtering Paths

--- workingWithExpressJS/app.js
+++ workingWithExpressJS/app.js
@@ -5,12 +5,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin.js');
-const shopRoutes = require('./routes/shop.js')
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
