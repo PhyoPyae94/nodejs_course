841b4c38a6d606a0ed19a2d0c1e3499bf82bfa31
PhyoPyae94 <kirudozack@gmail.com>
2019-04-12 23:35:02 +0630

    adding a 404 error page

--- workingWithExpressJS/app.js
+++ workingWithExpressJS/app.js
@@ -13,6 +13,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>404 error <br/> Page not found.</h1>');
+});
+
+
 // use method allow us to add new middle ware function
 // the third argument, next is a function, a function that will be passed to this function by exprss_js
 
