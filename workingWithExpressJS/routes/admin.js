6e319a2ffa8ec8de5c111fa4aae321e2a018fc51
PhyoPyae94 <kirudozack@gmail.com>
2019-04-13 12:43:52 +0630

    creating and serving HTML page

--- workingWithExpressJS/routes/admin.js
+++ workingWithExpressJS/routes/admin.js
@@ -1,10 +1,12 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.send('<html><body><form action="/admin/add-product" method="POST"><input type="text" name="message"><button type="submit">Add product</button></form></body></html>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
--- workingWithExpressJS/routes/shop.js
+++ workingWithExpressJS/routes/shop.js
@@ -1,9 +1,13 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello express_js!</h1>');// send() allow us to send well a response
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
+
+// __dirname => routes, view => sibling folder 
 
 module.exports = router;
